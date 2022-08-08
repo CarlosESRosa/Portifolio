0b1d75247ba4e8a478f9d99be096b4cad58d4599
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-08-08 19:33:38 -0300

    feat: :sparkles: responsividade projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './MediaQuery.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Profile from './components/Profile';
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,20 +6,26 @@ import blogsApi from '../img/blogsApi.png'
 export default class Projects extends Component {
   render() {
     return (
-      <div className='projects-box container' id='projects-box'>
+      <div className='container' id='projects-box'>
         <h1 className='section-title'>Projects</h1>
-        <div className='projects'>
-          <div className='my-project'>
-            <h4>Trybetunes</h4>
-            <a href="https://carlosesrosa.github.io/Trybetunes/" target="_blank" rel="noreferrer"><img src={trybetunes}  alt='trybetunes project'/></a>
+        <div class="row">
+          <div className="col-12 mb-5 col-md-6 col-xl-4">
+            <div className='my-project'>
+              <h4>Trybetunes</h4>
+              <a href="https://carlosesrosa.github.io/Trybetunes/" target="_blank" rel="noreferrer"><img src={trybetunes}  alt='trybetunes project'/></a>
+            </div>
           </div>
-          <div className='my-project'>
-            <h4>Rosashopping</h4>
-            <a href='https://carlosesrosa.github.io/Rosa-shopping/' target="_blank" rel="noreferrer"><img src={rosaShopping}  alt='rosa shopping project'/></a>
+          <div className="col-12 mb-5 col-md-6 col-xl-4">
+            <div className='my-project'>
+              <h4>Rosashopping</h4>
+              <a href='https://carlosesrosa.github.io/Rosa-shopping/' target="_blank" rel="noreferrer"><img src={rosaShopping}  alt='rosa shopping project'/></a>
+            </div>
           </div>
-          <div className='my-project'>
-            <h4>Blogs Api</h4>
-            <a href='https://github.com/CarlosESRosa/Blogs-api' target="_blank" rel="noreferrer"> <img src={blogsApi}  alt='Blogs api project'/></a>
+          <div className="col-12 col-md-6 col-xl-4">
+            <div className='my-project'>
+              <h4>Blogs Api</h4>
+              <a href='https://github.com/CarlosESRosa/Blogs-api' target="_blank" rel="noreferrer"> <img src={blogsApi}  alt='Blogs api project'/></a>
+            </div>
           </div>
         </div>
       </div>
