2ae0ccb01eebd47b193f146a601bcd26f6f1f0c5
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-08-10 11:11:42 -0300

    feat: :art: improve css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,6 +15,7 @@ export default class Profile extends Component {
               <h3>Full Stack Developer</h3>
               <p>Por ter sido cercado de tecnologia na infância e adolescência me encontrei na área de TI, estava estudando na Universidade Federal de Itajubá (UNIFEI), porém, optei por abandonar minha vaga e ingressar na Trybe, acreditando ser a melhor opção visando futuramente me tornar um Desenvolvedor Full Stack. 
               Atualmente estudo Desenvolvimento de Software na Trybe. Terminei meus estudos sobre Front-end onde utilizei principalmente React, Redux e Bootstrap, no momento estou estudando Back-end onde estou aprendendo Docker, Node, express, entre outras ferramentas.</p>
+              <h4>Tecnologias:</h4>
               <div className='tools-box'>
                 <img align="center" alt="React" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
                 <img align="center" alt="Redux" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
@@ -24,13 +25,9 @@ export default class Profile extends Component {
                 <img align="center" alt="Javascript" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
                 <img align="center" alt="Typescript" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
                 <img align="center" alt="Jest" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
-              </div>
-              <div className='tools-box'>
                 <img align="center" alt="Node" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
                 <img align="center" alt="Express" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
                 <img align="center" alt="Sequelize" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" />
-                <img align="center" alt="Javascript" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img align="center" alt="Typescript" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
                 <img align="center" alt="Docker" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
                 <img align="center" alt="MySQl" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" />
                 <img align="center" alt="MongoDb" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
