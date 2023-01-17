de751006d5bddce819868dbc4a05454bc003e89f
Carlos Rosa <carlosrosa1001@gmail.com>
2023-01-17 12:02:57 -0300

    feat: :sparkles: add new project to portifolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import trybetunes from "../img/trybetunes3.png";
 import rosaShopping from "../img/rosaShopping.png";
 import blogsApi from "../img/blogsApi.png";
 import carteiraVirtual from "../img/carteiraVirtual.png";
+import pokedex from "../img/pokedex.png";
 
 export default class Projects extends Component {
   render() {
@@ -19,27 +20,133 @@ export default class Projects extends Component {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img src={carteiraVirtual} alt="Carteira virtual project" />
+                  <img
+                    src={carteiraVirtual}
+                    alt="Carteira virtual project"
+                    className="project-img"
+                  />
+                  <div className="project-stacks">
+                    <img
+                      align="center"
+                      alt="React"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Typescript"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Node"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Docker"
+                      height="30"
+                      width="40"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+                    />
+                  </div>
+                </a>
+              </div>
+              <div className="my-project">
+                <h4>Pokedex</h4>
+                <a
+                  href="https://github.com/CarlosESRosa/Pokedex"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={pokedex}
+                    alt="pokedex project"
+                    className="project-img"
+                  />
+                  <div className="project-stacks">
+                    <img
+                      align="center"
+                      alt="React"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Javascript"
+                      height="30"
+                      width="40"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Node"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Docker"
+                      height="30"
+                      width="40"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+                    />
+                  </div>
                 </a>
               </div>
               <div className="my-project">
                 <h4>Trybetunes</h4>
                 <a
-                  href="https://carlosesrosa.github.io/Trybetunes/"
+                  href="https://github.com/CarlosESRosa/Trybetunes"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img src={trybetunes} alt="trybetunes project" />
+                  <img
+                    src={trybetunes}
+                    alt="trybetunes project"
+                    className="project-img"
+                  />
+                  <div className="project-stacks">
+                    <img
+                      align="center"
+                      alt="React"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Javascript"
+                      height="30"
+                      width="40"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                    />
+                  </div>
                 </a>
               </div>
+
               <div className="my-project">
-                <h4>Rosashopping</h4>
+                <h4>Blogs Api</h4>
                 <a
-                  href="https://carlosesrosa.github.io/Rosa-shopping/"
+                  href="https://github.com/CarlosESRosa/Blogs-api"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img src={rosaShopping} alt="rosa shopping project" />
+                  {" "}
+                  <img
+                    src={blogsApi}
+                    alt="Blogs api project"
+                    className="project-img"
+                  />
+                  <div className="project-stacks">
+                    <img
+                      align="center"
+                      alt="React"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Javascript"
+                      height="30"
+                      width="40"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                    />
+                  </div>
                 </a>
               </div>
               <div className="my-project">
@@ -50,7 +157,25 @@ export default class Projects extends Component {
                   rel="noreferrer"
                 >
                   {" "}
-                  <img src={blogsApi} alt="Blogs api project" />
+                  <img
+                    src={blogsApi}
+                    alt="Blogs api project"
+                    className="project-img"
+                  />
+                  <div className="project-stacks">
+                    <img
+                      align="center"
+                      alt="React"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Javascript"
+                      height="30"
+                      width="40"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                    />
+                  </div>
                 </a>
               </div>
               <div className="my-project">
@@ -61,18 +186,25 @@ export default class Projects extends Component {
                   rel="noreferrer"
                 >
                   {" "}
-                  <img src={blogsApi} alt="Blogs api project" />
-                </a>
-              </div>
-              <div className="my-project">
-                <h4>Blogs Api</h4>
-                <a
-                  href="https://github.com/CarlosESRosa/Blogs-api"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {" "}
-                  <img src={blogsApi} alt="Blogs api project" />
+                  <img
+                    src={blogsApi}
+                    alt="Blogs api project"
+                    className="project-img"
+                  />
+                  <div className="project-stacks">
+                    <img
+                      align="center"
+                      alt="React"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    />
+                    <img
+                      align="center"
+                      alt="Javascript"
+                      height="30"
+                      width="40"
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                    />
+                  </div>
                 </a>
               </div>
             </div>
