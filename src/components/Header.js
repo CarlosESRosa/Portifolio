1d4f52c7f69273b4688cf01b8d6d21990c4ee018
Carlos Rosa <carlosrosa1001@gmail.com>
2023-01-18 12:58:36 -0300

    fix: :sparkles: Fix header toogle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react'
+// .my-header navimport React, { Component } from "react";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div className='my-header container'>
-        <nav class="navbar navbar-expand-lg navbar-dark">
-          <div>
-            <a class="navbar-brand" href="#" id='header-icon'>Resume</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          </div>
-          <div class="collapse navbar-collapse icons-header" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
+export default function Header() {
+  return (
+    <header className="header">
+      <nav className="navbar navbar-expand-md ">
+        <div className="container">
+          <a className="navbar-brand" href="#" id="header-icon">
+            Resume
+          </a>
+          <button
+            className="navbar-toggler "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link mx-1 fs-4" href="home">
+                  Home
+                </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#profile-box">About</a>
+              <li className="nav-item">
+                <a className="nav-link mx-1 fs-4" href="#profile-box">
+                  About
+                </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#projects-box">Projects</a>
+              <li>
+                <a className="nav-link mx-1 fs-4" href="#projects-box">
+                  Projects
+                </a>
               </li>
             </ul>
           </div>
-        </nav>
-      </div>
-    )
-  }
+        </div>
+      </nav>
+    </header>
+  );
 }
